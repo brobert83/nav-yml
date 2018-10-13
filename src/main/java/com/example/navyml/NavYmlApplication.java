0b264704a1773b5b00ec633ebf277e45268520ba
Robert Baboi <robert.baboi@gmail.com>
2018-10-13 15:56:42 +0100

    copy-pasta

---
+++ src/main/java/com/example/navyml/NavYmlApplication.java
@@ -0,0 +1,12 @@
+package com.example.navyml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavYmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavYmlApplication.class, args);
+	}
+}
